159213346dc5d973c52b8f39bd1e43b28353c309
Babak Khorramfar <info.webschoool@gmail.com>
2025-05-28 10:11:21 +0330

    Register Service models in admin panel

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ServiceCategory, ServiceRequest
 
-# Register your models here.
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = ("customer", "category", "city", "status", "created_at")
+    list_filter = ("status", "city", "category")
+    search_fields = ("customer__username", "city", "description")
+    ordering = ("-created_at",)
